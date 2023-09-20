d895624e50ab11806a3f8a19d808a303c0227995
Huy Anh Nguyen <huyanh995@gmail.com>
2023-09-20 14:00:30 -0400

    Finish 1a

--- hw1/hw1collisionloadingscript.sql
+++ hw1/hw1collisionloadingscript.sql
@@ -1,9 +1,9 @@
-LOAD FROM "/home/huyanh/todb/HW1/NYPD_Motor_Vehicle_Collisions2017.csv"
+LOAD FROM "/home/huyanh/todb/hw1/NYPD_Motor_Vehicle_Collisions2017.csv"
 OF DEL
 METHOD P (
     1, 2, 4, 5, 6, 19, 20, 24, 25, 26
 )
-MESSAGES "/home/huyanh/todb/HW1/msg/loadmsg.txt"
+MESSAGES "/home/huyanh/todb/hw1/msg/loadmsg.txt"
 INSERT INTO cse532.collision (
 --     TO_DATE(date, 'MM/DD/YYYY'),
     date,
@@ -16,4 +16,4 @@ INSERT INTO cse532.collision (
     unique_key,
     vehicle_type_code_1,
     vehicle_type_code_2
-)
+);
