ea65032878a772f34fac7d165fd767ff01bfe676
Huy Anh Nguyen <huyanh995@gmail.com>
2023-09-20 15:25:10 -0400

    Add primary key for Q1

--- hw1/hw1collisionloadingscript.sql
+++ hw1/hw1collisionloadingscript.sql
@@ -3,10 +3,8 @@ OF DEL
 METHOD P (
     1, 2, 4, 5, 6, 19, 20, 24, 25, 26
 )
--- SKIPCOUNT 1
 -- MESSAGES "/home/dbhuyanh/msg/loadmsg.txt" -- DataGrip can display the error in console already
 INSERT INTO cse532.collision (
---     TO_DATE(date, 'MM/DD/YYYY'),
     date,
     time,
     zip_code,
--- hw1/hw1createcollisiontable.sql
+++ hw1/hw1createcollisiontable.sql
@@ -2,11 +2,11 @@ CREATE TABLE cse532.collision (
     date                            DATE,
     time                            TIME,
     zip_code                        SMALLINT, -- NY Zipcode in range [6390, 14975]
-    latitude                        DECIMAL, -- [-90, +90], instead of FLOAT for accuracy (float approx)
-    longitude                       DECIMAL, -- [-180, +180]
-    contributing_factor_vehicle1    VARCHAR(100),
-    contributing_factor_vehicle2    VARCHAR(100),
-    unique_key                      INTEGER,
-    vehicle_type_code_1             VARCHAR(100),
-    vehicle_type_code_2             VARCHAR(100)
+    latitude                        DECIMAL(15, 3), -- [-90, +90], instead of FLOAT for accuracy (float approx)
+    longitude                       DECIMAL(15, 3), -- [-180, +180]
+    contributing_factor_vehicle1    VARCHAR(200),
+    contributing_factor_vehicle2    VARCHAR(200),
+    unique_key                      INTEGER NOT NULL PRIMARY KEY, -- assign as primary key and not null
+    vehicle_type_code_1             VARCHAR(200),
+    vehicle_type_code_2             VARCHAR(200)
 )
