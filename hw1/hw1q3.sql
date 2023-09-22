2cd1a376af1bd4e01938996e44ed2abe4c9725c7
Huy Anh Nguyen <huyanh995@gmail.com>
2023-09-22 19:58:17 -0400

    Fix hw1q3

--- hw1/hw1q3.sql
+++ hw1/hw1q3.sql
@@ -1,9 +1,9 @@
-WITH collision_over_location AS (
+WITH coll_location AS (
     -- First get 10 longitude, latitude with first most collisions
     SELECT
         latitude,
         longitude,
-        COUNT(*) AS collision_count
+        COUNT(*) AS coll_count
     FROM cse532.collision
     WHERE
         -- Exclude all records with NULL for latitude, longitude, zipcode
@@ -11,20 +11,20 @@ WITH collision_over_location AS (
         latitude IS NOT NULL AND
         longitude IS NOT NULL
     GROUP BY latitude, longitude
-    ORDER BY collision_count DESC
+    ORDER BY coll_count DESC
     LIMIT 10
 ),
 result AS (
     -- Then, get all zipcodes correspond with 10 (latitude, longitude)
     SELECT DISTINCT
-        collision.zip_code,
-        collision_over_location.latitude,
-        collision_over_location.longitude
-    FROM cse532.collision
-    JOIN collision_over_location
-    ON collision.longitude = collision_over_location.longitude AND
-       collision.latitude = collision_over_location.latitude
-    WHERE collision.zip_code IS NOT NULL
+        coll.zip_code,
+        coll_location.latitude,
+        coll_location.longitude
+    FROM cse532.collision AS coll
+    JOIN coll_location
+    ON coll.longitude = coll_location.longitude AND
+       coll.latitude = coll_location.latitude
+    WHERE coll.zip_code IS NOT NULL
 )
 -- (Just for displaying/ Optional)
 -- Last, group them together in case one location has multiple zipcodes.
@@ -37,6 +37,4 @@ FROM
 GROUP BY
     result.latitude,
     result.longitude;
-
-git filter-branch --index-filter "git rm --cached --ignore-unmatch /Users/huyanh/Documents/todb/hw1/data/Motor_Vehicle_Collisions_-_Crashes.csv" --prune-empty -f -- --all
 
