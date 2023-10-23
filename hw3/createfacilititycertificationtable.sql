ba41bed7599ffbed936fb76cbdd7b3d3e0b2f208
Dell_TODB <huyanh995@gmail.com>
2023-10-23 02:26:43 -0400

    Finish HW3 Q2

--- hw3/createfacilititycertificationtable.sql
+++ hw3/createfacilititycertificationtable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE cse532.facilitycertification(
-    FacilityID          VARCHAR(16) NOT NULL
+    FacilityID          VARCHAR(16) NOT NULL -- Not primary key bcs one facility may have multiple certifications
     ,FacilityName       VARCHAR(128)
     ,Description        VARCHAR(128)
     ,AttributeType      VARCHAR(16)
---
+++ hw3/nearester.sql
@@ -0,0 +1,71 @@
+-- Dynamic radius version: double the radius until found some points within.
+WITH ONLYID AS (
+    SELECT
+        DISTINCT FACILITYID
+    FROM cse532.FACILITYCERTIFICATION AS CERT
+    WHERE
+        CERT.ATTRIBUTEVALUE = 'Intensive Care'
+),
+
+LOCATIONS AS (
+    SELECT
+        GEOLOC.GEOLOCATION
+    FROM ONLYID
+    JOIN cse532.FACILITY AS GEOLOC ON ONLYID.FACILITYID = GEOLOC.FACILITYID
+    WHERE
+        db2gse.ST_IsEmpty(GEOLOC.GEOLOCATION) = 0
+),
+
+RADIUS_SEARCH(RADIUS) AS (
+    -- Start from 5 km
+    SELECT 5.0 AS RADIUS FROM sysibm.sysdummy1
+
+    UNION ALL
+    -- Double the radius in each iteration
+    SELECT
+        RADIUS * 2
+    FROM RADIUS_SEARCH
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM LOCATIONS
+        WHERE
+            db2gse.ST_WITHIN(GEOLOCATION, db2gse.ST_BUFFER(db2gse.ST_POINT(40.891720, -73.016479, 1), RADIUS, 'KILOMETER')) = 1
+    )
+)
+-- Select the minimum radius that contains at least one point and also select the point and its distance from the anchor
+SELECT
+    LOCATIONS.GEOLOCATION,
+    db2gse.ST_DISTANCE(GEOLOCATION, db2gse.ST_POINT(40.891720, -73.016479, 1), 'KILOMETER') AS DISTANCE_KILOMETER
+FROM RADIUS_SEARCH
+JOIN LOCATIONS ON db2gse.ST_WITHIN(GEOLOCATION, db2gse.ST_BUFFER(db2gse.ST_POINT(40.891720, -73.016479, 1), RADIUS, 'KILOMETER')) = 1
+ORDER BY DISTANCE_KILOMETER
+LIMIT 1;
+
+
+-- =====================================================================================================================
+-- Simpler version: hard-coded radius to be 10 kilometers
+-- WITH ONLYID AS (
+--     SELECT
+--         DISTINCT FACILITYID
+--     FROM cse532.FACILITYCERTIFICATION AS CERT
+--     WHERE
+--         CERT.ATTRIBUTEVALUE = 'Intensive Care'
+-- ),
+--
+-- LOCATIONS AS (
+--     SELECT
+--         GEOLOC.GEOLOCATION
+--     FROM ONLYID
+--     JOIN cse532.FACILITY AS GEOLOC ON ONLYID.FACILITYID = GEOLOC.FACILITYID
+--     WHERE
+--         db2gse.ST_IsEmpty(GEOLOC.GEOLOCATION) = 0
+-- )
+--
+-- SELECT
+--     GEOLOCATION
+--     ,db2gse.ST_DISTANCE(GEOLOCATION, db2gse.ST_POINT(40.891720, -73.016479, 1), 'KILOMETER') AS DISTANCE_KILOMETER
+-- FROM LOCATIONS
+-- WHERE
+--     db2gse.ST_WITHIN(GEOLOCATION, db2gse.ST_BUFFER(db2gse.ST_POINT(40.891720, -73.016479, 1), 10, 'KILOMETER')) = 1
+-- ORDER BY DISTANCE_KILOMETER
+-- LIMIT 1;
